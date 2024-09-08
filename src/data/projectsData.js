045662b4892c47e15eaf5aa6b728a27cb45246e0
sumansauravmay <sumansauravmay12345@gmail.com>
2024-09-08 16:21:49 +0530

    projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -70,44 +70,7 @@ export const projectsData = [
     code: "https://github.com/omesh96/safe-bridge-5506",
     demo: "https://startling-youtiao-e9d129.netlify.app/",
     image: "https://i.postimg.cc/0yBLqp4Y/ayoa.jpg",
-   },
-  // {
-  //   id: 5,
-  //   projectName: "E-Commerce App",
-  //   projectDesc: "A Simple E-commerce application",
-  //   tags: ["React Native", "Firebase"],
-  //   code: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //   demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //   image: five,
-  // },
-  // {
-  //   id: 6,
-  //   projectName: "Uber Lite",
-  //   projectDesc: "Uber clone",
-  //   tags: ["Flutter"],
-  //   code: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //   demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //   image: six,
-  // },
-  // {
-  //   id: 7,
-  //   projectName: "Stock Market App",
-  //   projectDesc: "A simple stock market API app",
-  //   tags: ["React", "Redux", "Bootstrap"],
-  //   code: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //   demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //   image: seven,
-  // },
-  // {
-  //   id: 8,
-  //   projectName: "Car Pooling System",
-  //   projectDesc:
-  //     "The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution",
-  //   tags: ["Flutter", "React"],
-  //   code: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //   demo: "https://github.com/hhhrrrttt222111/developer-portfolio",
-  //   image: eight,
-  // },
+   }
 ];
 
 // Do not remove any fields.
